e7efea87af9f111843315ed4ff1a86b914babca5
Isaac Abotsi <abotsi@users.noreply.github.com>
2020-07-05 23:05:04 +0200

    Add a guess button in the title bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import * as React from 'react'
 import './styles.css'
 import Grid from './Grid'
 import { RecoilRoot } from 'recoil'
+import Button from '@material-ui/core/Button'
 
 export default function App() {
   return (
     <RecoilRoot>
-      <div className="App">
-        <h1>Hello Binairo</h1>
+      <div id="App">
+        <div id="toolbar">
+          <h1>Binairo Helper</h1>
+            <Button variant="contained">Guess</Button>
+        </div>
         <Grid />
       </div>
     </RecoilRoot>
