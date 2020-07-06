c922938e460374f15e78093f22a45aaa141d489d
Isaac Abotsi <abotsi@users.noreply.github.com>
2020-07-06 08:01:27 +0200

    chore: auto updated files

--- tests/utils.test.ts
+++
@@ -1,8 +0,0 @@
-import { cloneUpdateAt } from '../src/utils'
-
-it('should update an array item in a functional way', () => {
-  const base = ['a', 'a', 'a']
-  const copy = cloneUpdateAt(base, 1, 'b')
-  expect(copy).toEqual(['a', 'b', 'a'])
-  expect(base).toEqual(['a', 'a', 'a'])
-})
